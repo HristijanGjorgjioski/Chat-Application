2e5c4033801fd37541e4cf6c339e08642cae54c0
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-04 02:46:48 -0400

    Setting up useEffect in auth context

--- src/contexts/AuthContex.js
+++ src/contexts/AuthContex.js
@@ -6,3 +6,13 @@ import { auth } from 'firebase';
 const AuthContext = React.createContext();
 
 export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState({});
+    const history = useHistory();
+
+    useEffect(() => {
+
+    }, [user, history])
+}
