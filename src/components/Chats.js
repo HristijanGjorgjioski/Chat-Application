c5191a95e93e48b9907e2aed668f9cb38eeab7a8
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-04 02:59:55 -0400

    Working on Chats component

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine} from 'react-chat-engine';
+
+import { auth } from '../firebase';
 
 const Chats = () => {
     return (
-        <div>
-            
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Unichat
+                </div>
+                <div className="logout-tab">
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine />
         </div>
     )
 }
