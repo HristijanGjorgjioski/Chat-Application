594bfba88b7fa7a7f3a289423f67ebdfd8741aa8
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-04 02:49:40 -0400

    AuthContext is done

--- src/contexts/AuthContex.js
+++ src/contexts/AuthContex.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { auth } from 'firebase';
+import { auth } from '../firebase';
 
 const AuthContext = React.createContext();
 
@@ -13,6 +13,18 @@ export const AuthProvider = ({ children }) => {
     const history = useHistory();
 
     useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            setUser(user);
+            setLoading(false);
+            history.push('/chats')
+        })
+    }, [user, history]);
 
-    }, [user, history])
+    const value = { user };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
 }
