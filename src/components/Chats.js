3ca8264b4f9b8d95427a4aa43e190bd1ba278e09
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-05 02:09:47 -0400

    Debugging the app

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,82 +1,94 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { ChatEngine} from 'react-chat-engine';
+import React, { useRef, useState, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { ChatEngine } from 'react-chat-engine'
+import axios from 'axios'
 
-import { auth } from '../firebase';
 import { useAuth } from '../contexts/AuthContex';
-import axios from 'axios';
+import { auth } from "../firebase"
 
-const Chats = () => {
-    const [loading, setLoading] = useState(true)
-    const history = useHistory();
-    const { user } = useAuth();
+export default function Chats() {
+  const didMountRef = useRef(false)
+  const [ loading, setLoading ] = useState(true)
+  const { user } = useAuth()
+  const history = useHistory()
 
-    const handleLogout = async () => {
-        await auth.signOut();
-        history.push('/')
-    }
+  async function handleLogout() {
+    await auth.signOut()
+    history.push("/")
+  }
 
-    const getFile = async (url) => {
-        const response = await fetch(url);
-        const data = await response.blob();
-        return new File([data], "userPhoto.jpg", { type: 'image/jpg '});
-    }
+  async function getFile(url) {
+    let response = await fetch(url);
+    let data = await response.blob();
+    return new File([data], "test.jpg", { type: 'image/jpeg' });
+  }
 
-    useEffect(() => {
-        if(!user) {
-            history.push('/');
-            return;
-        }
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
 
-        axios.get('https://api.chatengine.io/users/me', {
-            headers: {
-                "project-id": "645c5a3d-e1aa-40f7-b767-0787117a8f96",
-                "user-name": user.email,
-                "user-secret": user.uid,
-            }
-        }).then(() => {
-            setLoading(false)
-        }).catch((err) => {
-            let formdata = new FormData();
-            formdata.append('email', user.email);
-            formdata.append('username', user.displayName);
-            formdata.append('secret', user.uid);
+      if (!user || user === null) {
+        history.push("/")
+        return
+      }
+      
+      // Get-or-Create should be in a Firebase Function
+      axios.get(
+        'https://api.chatengine.io/users/me/',
+        { headers: { 
+          "project-id": '784bdb9e-8724-4f63-8ab6-3c10d59f74a7',
+          "user-name": user.email,
+          "user-secret": user.uid
+        }}
+      )
 
-            getFile(user.photoURL)
-                .then((avatar) => {
-                    formdata.append('avatrar', avatar, avatar.name);
+      .then(() => setLoading(false))
 
-                    axios.post('htpps://api.chatengine.io/users/', 
-                        formdata,
-                        { headers: { "private-key": "e7766a70-5bc7-40b6-b702-60e7a6f51d93" } }
-                    )
-                    .then(() => setLoading(false))
-                    .catch((error) => console.log(error))
-                })
+      .catch(e => {
+        let formdata = new FormData()
+        formdata.append('email', user.email)
+        formdata.append('username', user.email)
+        formdata.append('secret', user.uid)
+
+        getFile(user.photoURL)
+        .then(avatar => {
+          formdata.append('avatar', avatar, avatar.name)
+
+          axios.post(
+            'https://api.chatengine.io/users/',
+            formdata,
+            { headers: { "private-key": "e7766a70-5bc7-40b6-b702-60e7a6f51d93" }}
+          )
+          .then(() => setLoading(false))
+          .catch(e => console.log('e', e.response))
         })
-    }, [user, history])
+      })
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    if (!user || loading) return 'Loading...';
+    }
+  }, [user, history])
+  
 
-    return (
-        <div className="chats-page">
-            <div className="nav-bar">
-                <div className="logo-tab">
-                    Unichat
-                </div>
-                <div onClick={handleLogout} className="logout-tab">
-                    Logout
-                </div>
-            </div>
+  if (!user || loading) return <div />
 
-            <ChatEngine 
-                height="calc(100vh - 66px)"
-                projectID="645c5a3d-e1aa-40f7-b767-0787117a8f96"
-                userName={user.email}
-                userSecret={user.uid}
-            />
+  return (
+    <div className='chats-page'>
+      <div className='nav-bar'>
+        <div className='logo-tab'>
+          Unichat
         </div>
-    )
-}
 
-export default Chats
+        <div onClick={handleLogout} className='logout-tab'>
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine 
+        height='calc(100vh - 66px)'
+        projectID='645c5a3d-e1aa-40f7-b767-0787117a8f96'
+        userName={user.email}
+        userSecret={user.uid}
+      />
+    </div>
+  )
+}
