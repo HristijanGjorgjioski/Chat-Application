0f3f7a431cd45c5cd778924015470a41b7665a57
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-05 02:44:46 -0400

    Creating folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,18 @@ import Cookies from 'universal-cookie'
 
 const apiKey = 'szb7j6e4kv2h';
 
+const client = StreamChat.getInstance(apiKey);
+
 const App = () => {
   return (
-    <div>
-      <h1>Medical Pager Chat Application</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team dark">
+        <ChannelListContainer 
+        
+        />
+
+        <ChannelContainer />
+      </Chat>
     </div>
   )
 }
