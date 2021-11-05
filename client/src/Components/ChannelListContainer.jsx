e4c839f652e32f90a7789a04f145a0b6ebcb0a7c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-05 02:55:42 -0400

    Started working on ChannelListCOntainer componetn

--- client/src/Components/ChannelListContainer.jsx
+++ client/src/Components/ChannelListContainer.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+// import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
+
+const SideBar = () => (
+    <div>
+        
+    </div>
+)
 
 const ChannelListContainer = () => {
     return (
