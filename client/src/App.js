255ec0ab3d8b055c40d91be6170087ca38c44ad6
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-05 02:42:42 -0400

    Initializing Chat in the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+
+const apiKey = 'szb7j6e4kv2h';
 
 const App = () => {
   return (
     <div>
-      
+      <h1>Medical Pager Chat Application</h1>
     </div>
   )
 }
