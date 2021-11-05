d3a3828c3bce970865e4d0511270f667362f503f
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-05 01:23:42 -0400

    Working with user profile and headers for chatengine

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { ChatEngine} from 'react-chat-engine';
 
 import { auth } from '../firebase';
 import { useAuth } from '../contexts/AuthContex';
+import axios from 'axios';
 
 const Chats = () => {
+    const [loading, setLoading] = useState(true)
     const history = useHistory();
     const { user } = useAuth();
 
@@ -13,6 +15,24 @@ const Chats = () => {
         await auth.signOut();
         history.push('/')
     }
+
+    useEffect(() => {
+        if(!user) {
+            history.push('/');
+            return;
+        }
+
+        axios.get('https://api.chatengine.io/users/me', {
+            headers: {
+                "project-id": "645c5a3d-e1aa-40f7-b767-0787117a8f96",
+                "user-name": user.email,
+                "user-secret": user.uid,
+            }
+        })
+        .then(() => {
+            setLoading(false)
+        })
+    })
 
     return (
         <div className="chats-page">
