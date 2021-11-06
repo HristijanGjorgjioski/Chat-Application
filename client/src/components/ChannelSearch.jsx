e51621caf732a466212e42943fda4c70cdb38be1
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-06 01:08:34 -0400

    Handeling events onSearch

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useChatContext } from 'stream-chat-react'
 
 import { SearchIcon } from '../assets'
 
 const ChannelSearch = () => {
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false)
+
+    const getChannels = async (text) => {
+        try {
+            // TODO: fetch channels
+        } catch (error) {
+            setQuery('')
+        }
+    }
+
+    const onSearch = (e) => {
+        e.preventDefault();
+
+        setLoading(true)
+        setQuery(e.target.value)
+        getChannels(e.target.value)
+    }
+
     return (
         <div className="channel-search__container">
             <div className="channel-search__input__wrapper">
@@ -14,8 +33,8 @@ const ChannelSearch = () => {
                     className="channel-search__input__text" 
                     placeholder="search" 
                     type="text" 
-                    value={''}
-                    onChange={''} 
+                    value={query}
+                    onChange={onSearch} 
                 />
             </div>
         </div>
