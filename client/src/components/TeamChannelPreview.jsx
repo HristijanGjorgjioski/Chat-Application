664023e09e4ff7a79f2500f0ead25885b3cdbb06
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-07 10:40:52 -0500

    Working on TeamChannelPreview

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useChatContext } from 'stream-chat-react'
+import { Avatar, useChatContext } from 'stream-chat-react'
 
 const TeamChannelPreview = ({ channel, type }) => {
     const { channel: activeChannel, client } = useChatContext();
@@ -10,13 +10,32 @@ const TeamChannelPreview = ({ channel, type }) => {
         </p>
     );
 
-    const DirectPreview = () => (
-        <div></div>
-    );
+    const DirectPreview = () => {
+        const members = Object.values(channel.state.members.filter(({ user }) => user.id !== client.userID));
+
+        return (
+            <div className="channel-preview_item single">
+                <Avatar 
+                    image={members[0]?.user?.image}
+                    name={members[0]?.user?.fullName}
+                    size={24}
+                />
+                <p>{members[0]?.user?.fullName}</p>
+            </div>
+        )
+    };
 
     return (
-        <div>
-            
+        <div className={
+            channel?.id === activeChannel?.id
+                ? 'channel-preview__wrapper__selected'
+                : 'channel-preview__wrapper'
+            }
+            onClick={() => {
+                console.log(channel)
+            }}
+        >
+            {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
         </div>
     )
 }
