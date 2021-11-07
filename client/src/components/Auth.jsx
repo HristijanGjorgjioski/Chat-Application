003f4e8bff33c3bb9f7d76e92aca5a65d668d261
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-07 10:53:18 -0500

    Creating Auth form

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Cookies from 'universal-cookie'
+import axios from 'axios'
+
+import signinImage from '../assets/signup.jpg'
 
 const Auth = () => {
+    const [isSignup, setIsSignup] = useState(false)
+
     return (
-        <div>
-            
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-container_fields-content">
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={() => {}}>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                            </div>
+                        )}
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
