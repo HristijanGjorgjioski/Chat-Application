c00758f89a8250fea5d59f4d90a9158b01113e39
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-08 10:04:54 -0500

    Working on signup controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,14 @@ const login = (req, res) => {
 }
 
 const signup = (req, res) => {
+    const {  } = req.body;
 
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error })
+    }
 }
 
 module.exports = { login, signup };
