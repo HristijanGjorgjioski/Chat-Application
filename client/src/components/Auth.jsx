f307f9b4dd92072c50fab16c3e45f2aedbcdc87c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-08 10:27:12 -0500

    Setting up cookies

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 import signinImage from '../assets/signup.jpg'
 
+const cookies = new Cookies;
+
 const initialState = {
     fullName: '',
     username: '',
@@ -28,9 +30,20 @@ const Auth = () => {
 
         const URL = 'http://localhost:5000/auth';
 
-        const { data } = await axios.post(`${URL}/${isSignup ? `signup` : 'login'}`, {
+        const { data: { token, userId, hashedPassword } } = await axios.post(`${URL}/${isSignup ? `signup` : 'login'}`, {
             username, password, fullName, phoneNumber, avatarURL
-        })
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
     }
 
     const switchMode = () => {
