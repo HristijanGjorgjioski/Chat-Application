0b197d36f62c81f896ba8ea92347270f7aebe785
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-08 10:14:04 -0500

    creating login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,20 +5,34 @@ const crypto = require('crypto')
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
-const api_id = process.env.STREAM_API_ID;
+const app_id = process.env.STREAM_API_ID;
 
 const login = (req, res) => {
+    try {
+        const { username, password } = req.body;
 
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key. api_secret);
+
+    } catch (error) {
+        
+    }
 }
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
     try {
         const { fullName, username, password, phoneNumber } = req.body;
 
         const userId = crypto.randomBytes(16).toString('hex');
 
-        const serverClient = connect(api_key, api_secret, app_id)
+        const serverClient = connect(api_key, api_secret, app_id);
         
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const token = serverClient.createUserToken(userId);
+
+        res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
+
     } catch (error) {
         console.log(error);
         res.status(500).json({ error })
