69386a6d9097f7ee3e69061c13fe420387a426b8
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-09 01:23:20 -0500

    Fixing some bug on backend

--- client/src/App.js
+++ client/src/App.js
@@ -15,14 +15,13 @@ const client = StreamChat.getInstance(apiKey);
 
 if(authToken) {
   client.connectUser({
-    token: cookies.get('token'),
-    username: cookies.get('username'),
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
     fullName: cookies.get('fullName'),
-    userId: cookies.get('userId'),
-    phoneNumber: cookies.get('phoneNumber'),
-    avatarURL: cookies.get('avatarURL'),
+    image: cookies.get('avatarURL'),
     hashedPassword: cookies.get('hashedPassword'),
-  })
+    phoneNumber: cookies.get('phoneNumber'),
+  }, authToken)
 }
 
 const App = () => {
@@ -32,7 +31,7 @@ const App = () => {
     <div className="app__wrapper">
       <Chat client={client} theme="team dark">
         <ChannelListContainer
-        
+          
         />
 
         <ChannelContainer />
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,15 @@
 const { connect } = require('getstream')
 const bcrypt = require('bcrypt')
-const StreamChat = require('stream-chat')
+const StreamChat = require('stream-chat').StreamChat;
 const crypto = require('crypto')
+
+require('dotenv').config()
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_API_ID;
 
-const login = (req, res) => {
+const login = async (req, res) => {
     try {
         const { username, password } = req.body;
 
