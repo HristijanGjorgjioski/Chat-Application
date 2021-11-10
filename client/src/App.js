e45103809904f436f39de37a2667b2619bc9f534
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-10 08:42:56 -0500

    Creating EmptyState component

--- client/src/App.js
+++ client/src/App.js
@@ -15,12 +15,12 @@ const client = StreamChat.getInstance(apiKey);
 
 if(authToken) {
   client.connectUser({
-      id: cookies.get('userId'),
-      name: cookies.get('username'),
-      fullName: cookies.get('fullName'),
-      image: cookies.get('avatarURL'),
-      hashedPassword: cookies.get('hashedPassword'),
-      phoneNumber: cookies.get('phoneNumber'),
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+    phoneNumber: cookies.get('phoneNumber'),
   }, authToken)
 }
 
